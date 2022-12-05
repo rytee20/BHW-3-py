53ab58ee287f0492973348dd6a9d14786adc6469
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-12-05 17:15:38 +0300

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -4,27 +4,42 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 pd.options.mode.chained_assignment = None
-train_data = pd.read_csv('train.csv')
+train_data = pd.read_csv('train.csv') # данные rain.csv
+train_data['Age'] = train_data['Age'].fillna(train_data['Age'].median()) # заполням возраст медианой
 
-train_data['Age'] = train_data['Age'].fillna(train_data['Age'].median())
+data_to_train = train_data[[ # здесь можно закомментить те параметры, на которые мы не будем обращать внимания при 
+    # предсказании или добавить новые 
+    'Pclass',
+    'Age',
+    'Fare',
+    'Sex']]
+data_to_train['Sex'] = np.where(data_to_train['Sex'] == 'female', 0, 1) # даем полу числовае значения
 
-data_to_train = train_data[['Pclass', 'Age', 'Sex']]
-data_to_train['Sex'] = np.where(data_to_train['Sex'] == 'female', 0, 1)
+test_data = pd.read_csv('test.csv')  # читаем файл test.csv
+test_data['Age'] = test_data['Age'].fillna(test_data['Age'].median()) # заполняем медианой возраст
+test_data['Fare'] = test_data['Fare'].fillna(test_data['Fare'].median()) # заполняем медианой цену билета
 
-test_data = pd.read_csv('test.csv')
-test_data['Age'] = test_data['Age'].fillna(test_data['Age'].median())
+data_to_test = test_data[[ # здесь можно закомментить те параметры, на которые мы не будем обращать внимания при 
+    # предсказании или добавить новые 
+    'Pclass',
+    'Age',
+    'Fare',
+    'Sex']]
+data_to_test['Sex'] = np.where(data_to_test['Sex'] == 'female', 0, 1) # опять дем пол численным
 
-data_to_test = test_data[['Pclass', 'Age', 'Sex', 'Fare']]
-data_to_test['Sex'] = np.where(data_to_test['Sex'] == 'female', 0, 1)
-
-expected_data = train_data['Survived']
+expected_data = train_data['Survived'] # нам нужео предсказать выживаемисть
 rf = RandomForestClassifier(n_estimators=100)
 rf.fit(data_to_train, expected_data)
-pred = rf.predict(data_to_test)
+prediction = rf.predict(data_to_test) # предсказали
 
-with open('output.csv', 'w') as f:
+with open('output.csv', 'w') as f: # выводим
     f.write("PassengerId,Survived\n")
     i = 892
-    for p in pred:
+    for p in prediction:
         f.write(str(i) + "," + str(p) + "\n")
         i += 1
+
+# Точность с учетом Пола, Класса и  Возраста (заполнен медианой) 0,64114
+# Точность с учетом Пола, Класса и Цены билета (заполнена медианой) 0,76315
+# Точность с учетом Пола, Класса, Цены билета (заполнена медианой) и Возраста (заполнен медианой) 0,74401
+# Точность с учетом Пола и Класса 0,77511
