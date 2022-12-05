a58addfd1b1577b0166f924686f1b94b1ae0694f
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-12-05 16:48:06 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -3,6 +3,7 @@ import pandas as pd
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 all_data = pd.concat([train_data, test_data])
+
 lastnames={}
 firstnames={}
 
@@ -13,8 +14,15 @@ for person in all_data.iterrows():
 
 for person in all_data.iterrows():
     person = person[1]
-    firstname = person.Name.split(', ')[1]
-    firstname = firstname.split(' ')[1]
+
+    if(person.Name.find("(")!=-1):
+        firstname = person.Name.split("(")[1]
+        firstname = firstname.split(" ")[0]
+
+    else:
+        firstname = person.Name.split(", ")[1]
+        firstname = firstname.split(" ")[1]
+
     firstnames[firstname] = firstnames.setdefault(firstname, 0) + 1
 
 list_lastnames = list(lastnames.items())
@@ -26,7 +34,6 @@ print("ТОП-10 популярных фамилий (в порядке убыв
 for i in range(10):
     print(list_lastnames[-i - 1][0])
 
-print("ТОП-10 популярных имен (среди первых) (в порядке убываня популярности):")
+print("\nТОП-10 популярных имен (среди первых) (в порядке убываня популярности):")
 for i in range(10):
     print(list_firstnames[-i - 1][0])
-    
